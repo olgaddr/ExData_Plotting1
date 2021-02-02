ec3558af0e40edbf84e8c3390991ddd9a6caf48d
olgaddr <59804164+olgaddr@users.noreply.github.com>
2021-02-02 03:46:49 +0200

    Update plot2.R

--- figure/plot2.R
+++ figure/plot2.R
@@ -1,4 +1,10 @@
-data=read.table(file.choose(),sep = ";", header = TRUE, na.strings = "?")
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data <- read.table(unz(temp, "household_power_consumption.txt"),sep = ";", header = TRUE, na.strings = "?")
+unlink(temp)
+
+#data=read.table(file.choose(),sep = ";", header = TRUE, na.strings = "?")
+
 data <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
 data$datetime <- as.POSIXct(paste(data$Date, data$Time))
 Sys.setlocale("LC_TIME", "C")
