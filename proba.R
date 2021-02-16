edc335850b361490bfac4fcce0f7526fc0a7687a
Olga <olga.dunajeva@gmail.com>
2021-02-16 14:31:18 +0200

    test

--- proba.R
+++ proba.R
@@ -1,2 +1,4 @@
 print("This file was created within RStudio")
 print("This file was created within RStudio")
+print("This file was created within RStudio")
+print("This file was created within RStudio")
